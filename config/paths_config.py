529a74f7119a7f985fe239912c86aaef86e90efe
bsds1006238 <21f1006238@ds.study.iitm.ac.in>
2026-04-05 14:52:07 +0530

    Added jobs to workflow

--- config/paths_config.py
+++ config/paths_config.py
@@ -1,9 +1,11 @@
 import os
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parents[1]
 
 ################### DATA INGESTION #################
 
-RAW_DIR = 'artifacts/raw'
+RAW_DIR = BASE_DIR / "artifacts" / "raw"
 RAW_FILE_PATH = os.path.join(RAW_DIR,"iris.csv")
 TRAIN_FILE_PATH = os.path.join(RAW_DIR,"train.csv")
 TEST_FILE_PATH = os.path.join(RAW_DIR,"test.csv")
@@ -13,11 +15,12 @@ CONFIG_PATH = 'config/config.yaml'
 
 ################ Data Processing ##########
 
-PROCESSED_DIR = 'artifacts/processed'
+
+PROCESSED_DIR = BASE_DIR / 'artifacts' / 'processed'
 PROCESSED_TRAIN_DATA_PATH = os.path.join(PROCESSED_DIR,"processed_train.csv")
 PROCESSED_TEST_DATA_PATH = os.path.join(PROCESSED_DIR,"processed_test.csv")
 
 
 ################ MODEL OUTPUT PATH ##########
 
-MODEL_OUTPUT_PATH = 'artifacts/model/rf_model.pkl'
+MODEL_OUTPUT_PATH = BASE_DIR / 'artifacts' / 'model' / 'rf_model.pkl'
